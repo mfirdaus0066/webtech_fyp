4494f33f73f6a6a987a92c3f5806d96853906e14
Ehsan <ehsan150706@gmail.com>
2025-06-08 23:59:35 +0800

    ss

--- javascript/cat-bio.js
+++ javascript/cat-bio.js
@@ -15,7 +15,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
         1:{ name: "snowy", age: "4 months", breed: "british Short hair", color: "Light brown & white", place: "Selangor", image: "../image/Snowy.jpg", status: "Available", address: "https://maps.app.goo.gl/ipyoSrkXGwYMEZm17" },
 
-<<<<<<< HEAD
         2:{ name: "Milo", age: "2 years", breed: "American Shorthair", color: "Grey Tabby", place: "Pahang", image: "../image/Gato.jpg", status: "Adopted" },
 
         3:{ name: "Ginger", age: "3 years", breed: "Domestic Shorthair", color: "Orange Tabby", place: "Selangor", image: "../image/Ginger.jpg", status: "Available" },
@@ -33,25 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
         9:{ name: "Mochi", age: "2 months", breed: "Munchkin", color: "Orange and white", place: "Pahang", image: "../image/Munchkin.jpg", status: "Available" },
 
         10:{ name: "Daisy", age: "2 years", breed: "Siberian", color: "White", place: "Terengganu", image: "../image/Daisy.jpg", status: "Available" }
-=======
-        2:{ name: "Milo", age: "2 years", breed: "American Shorthair", color: "Grey tabby", place: "Johor", image: "../image/Gato.jpg", status: "Adopted", address: "https://maps.app.goo.gl/3WH2yJWFLejHuY1T8"},
-
-        3:{ name: "Ginger", age: "3 year", breed: "siamese", color: "orange", place: "penang", image: "../image/Ginger.jpg", status: "Available", address: "https://maps.app.goo.gl/nCj2yTp6kZ4pr2gY9" },
-
-        4:{ name: "Lune", age: "4 months", breed: "ragdoll", color: "black", place: "Selangor", image: "../image/Luna.jpg", status: "Available", address: "https://maps.app.goo.gl/ipyoSrkXGwYMEZm17" },
-
-        5:{ name: "oreo", age: "2 years", breed: "burmese", color: "calico", place: "Penang", image: "../image/Oreo.jpg", status: "Adopted", address: "https://maps.app.goo.gl/nCj2yTp6kZ4pr2gY9" },
-
-        6:{ name: "Bella", age: "1.5 year", breed: "scottish fold", color: "white", place: "Johor", image: "../image/Bella.jpg", status: "Available", address: "https://maps.app.goo.gl/3WH2yJWFLejHuY1T8" },
-
-        7:{ name: "coco", age: "5 months", breed: "sphynx", color: "brown", place: "Penang", image: "../image/Coco.jpg", status: "Available", address: "https://maps.app.goo.gl/nCj2yTp6kZ4pr2gY9" },
-
-        8:{ name: "Simba", age: "1 years", breed: "sphynx", color: "brown", place: "Johor", image: "../image/Simba.jpg", status: "Adopted", address: "https://maps.app.goo.gl/3WH2yJWFLejHuY1T8" },
-
-        9:{ name: "munchkin", age: "6 months", breed: "munchkin", color: "brown", place: "Selangor", image: "../image/Munchkin.jpg", status: "Available", address: "https://maps.app.goo.gl/ipyoSrkXGwYMEZm17" },
-
-        10:{ name: "Daisy", age: "2 years", breed: "sphynx", color: "brown", place: "Selangor", image: "../image/Daisy.jpg", status: "Available", address: "https://maps.app.goo.gl/ipyoSrkXGwYMEZm17" }
->>>>>>> bff25073065af1d03f58ce75ecd570f43f7829cd
+       
     }
 
     const params = new URLSearchParams(window.location.search);
