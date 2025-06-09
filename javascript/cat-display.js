9e07ccc46d54f56f0a4f288e252204a79b585b8e
firdaus <mfirdaus120606@gmail.com>
2025-06-09 18:07:01 +0800

    k

--- javascript/cat-display.js
+++ javascript/cat-display.js
@@ -1,4 +1,15 @@
-function catDisplay()
+function catDisplay(currentPage)
 {
-    
+    const display = document.getElementById("cat-cards");
+    const catLimit = 10;
+    const start = (currentPage - 1) * catLimit;
+    const end = start + catLimit;
+
+    const limitDisplay = allCats.slice(start, end);
+    const productContainer = document.getElementById("cat-cards");
+
+    productContainer.innerHTML = "";
+
+    displayCat.forEach ()
+
 };
